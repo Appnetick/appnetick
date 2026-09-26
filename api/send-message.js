235dd1de297ae0b448456f5d6241ac8f3253785f
Appnetick <gorurai272@gmail.com>
2026-09-26 08:24:54 +0530

    Update send-message.js

--- api/send-message.js
+++ api/send-message.js
@@ -24,17 +24,51 @@ function json(res, status, data) {
     return res.status(status).json(data);
 }
 
+let lastPushTime = 0;
+let lastRandChars = [];
+
 function generatePushKey() {
-    const chars =
+    const PUSH_CHARS =
         "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
 
-    let key = "";
+    let now = Date.now();
+    const duplicateTime = now === lastPushTime;
 
-    for (let i = 0; i < 20; i++) {
-        key += chars[Math.floor(Math.random() * chars.length)];
+    lastPushTime = now;
+
+    let timeStampChars = new Array(8);
+
+    for (let i = 7; i >= 0; i--) {
+        timeStampChars[i] = PUSH_CHARS.charAt(now % 64);
+        now = Math.floor(now / 64);
     }
 
-    return key;
+    let id = timeStampChars.join("");
+
+    if (!duplicateTime) {
+        lastRandChars = [];
+
+        for (let i = 0; i < 12; i++) {
+            lastRandChars[i] =
+                Math.floor(Math.random() * 64);
+        }
+    } else {
+        let i;
+
+        for (i = 11; i >= 0 && lastRandChars[i] === 63; i--) {
+            lastRandChars[i] = 0;
+        }
+
+        if (i >= 0) {
+            lastRandChars[i]++;
+        }
+    }
+
+    for (let i = 0; i < 12; i++) {
+        id += PUSH_CHARS.charAt(lastRandChars[i]);
+    }
+
+    return id;
 }
 
 function getUsername(user) {
